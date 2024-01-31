b624d5cf80a3283ad3f4ddd356b7244b8c4a4fbf
Jp_Sena <141283252+tugabaiano@users.noreply.github.com>
2024-01-31 09:41:59 -0300

    Update projeto java I.java

--- projeto java I.java
+++ projeto java I.java
@@ -1,13 +1,11 @@
-// Definindo a classe "Pessoa"
 class Carro {
-    // Atributos da classe
-    private String marca; // Atributo privado para encapsulamento
-    private int anoDeFabricacao;   // Atributo privado para encapsulamento
+
+    private String marca; 
+    private int anoDeFabricacao;   
     private String modelo;
     private int velocidadeAtual;
     private String cor;
     
-    // Método construtor da classe para inicializar os atributos
     Carro(String marca, int anoDeFabricacao, String modelo, int velocidadeAtual, String cor) {
         this.marca = marca;
         this.anoDeFabricacao = anoDeFabricacao;
@@ -16,45 +14,36 @@ class Carro {
         this.cor = cor;
     }
     
-    // Método getter para obter ...
     public String getMarca() {
         return marca;
     }
     
-    // Método getter para obter ...
-     public int getAnoDeFabricacao() {
+    public int getAnoDeFabricacao() {
         return anoDeFabricacao;
     }
     
-    // Método getter para obter ...
-     public String getModelo() {
+    public String getModelo() {
         return modelo;
     }
     
-    // Método getter para obter ...
-     public int getVelocidadeAtual() {
+    public int getVelocidadeAtual() {
         return velocidadeAtual;
     } 
     
-    // Método getter para obter ...
     public String getCor() {
         return cor;
     }
-    
- 
 }
 
-// Classe principal que contém o método main
 public class Main {
     public static void main(String[] args) {
-        // Criando um objeto da classe Pessoa chamado "pessoa1"
-        Carro carro1 = new Carro("BMW", 2023,"660x", 290,"Preto");
+        // Criando um objeto da classe Carro chamado "carro1"
+        Carro carro1 = new Carro("BMW", 2023, "660x", 290, "Preto");
         
-        // Usando os métodos getters para obter e imprimir as informações da pessoa
-        System.out.println("Marca: " + carro1.getMarca()); // Obtém e imprime o nome da pessoa
-        System.out.println("Ano de fabricacao: " + carro11.getAnoDeFabricacao()); // Obtém e imprime a idade da pessoa
-        System.out.println("Modelo: " + carro11.getModelo());
-        System.out.println("Velocidade MAX: " + carro11.getVelocidadeAtual());
-        System.out.println("Cor: " + carro11.getCor());
+        System.out.println("Marca: " + carro1.getMarca());
+        System.out.println("Ano de fabricacao: " + carro1.getAnoDeFabricacao());
+        System.out.println("Modelo: " + carro1.getModelo());
+        System.out.println("Velocidade MAX: " + carro1.getVelocidadeAtual());
+        System.out.println("Cor: " + carro1.getCor());
     }
 }
